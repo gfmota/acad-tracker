fe21fcd4a71cf5c7ae392be1066f8ceb68493e87
gfmota <gabrielfmota134@gmail.com>
2023-09-16 00:54:33 -0300

    feat: add jest
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    setupFilesAfterEnv: ['./src/setupTests.ts'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    modulePaths: ['.'],
+    moduleNameMapper: {
+        '^.+\\.(css|less|scss|sass)$': '<rootDir>/tests/mock.ts',
+    },
+    testEnvironment: 'jest-environment-jsdom',
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: -10,
+        },
+    },
+}
