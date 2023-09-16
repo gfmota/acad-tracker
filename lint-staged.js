648e4c422a732a9241d9d0c24e22ec586b48a5d3
gfmota <gabrielfmota134@gmail.com>
2023-09-16 01:16:48 -0300

    ent

--- lint-staged.js
+++ lint-staged.js
@@ -2,7 +2,6 @@ module.exports = {
     '*.{js,jsx,ts,tsx}': [
         'eslint',
         'jest --bail --watchAll=false --passWithNoTests --findRelatedTests',
-        () => 'tsc-files --noEmit',
     ],
     '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
 }
