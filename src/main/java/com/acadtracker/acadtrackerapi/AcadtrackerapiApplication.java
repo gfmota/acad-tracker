b79cd4d35bf3272b87c8fe38ba6e21ef61f83a56
gfmota <gabrielfmota134@gmail.com>
2023-12-29 15:32:32 -0300

    first commit

---
+++ src/main/java/com/acadtracker/acadtrackerapi/AcadtrackerapiApplication.java
@@ -0,0 +1,13 @@
+package com.acadtracker.acadtrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadtrackerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcadtrackerapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acadtracker/acadtrackerapi/AcadtrackerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acadtracker.acadtrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadtrackerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
